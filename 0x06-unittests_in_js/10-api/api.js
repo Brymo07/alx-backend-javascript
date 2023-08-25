a5547f55fc4ef2f1d08ee0e60d1935513250172c
Brymo07 <felixowusu479@gmail.com>
2023-08-24 20:10:03 -0700

    unittests in js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,53 +1,36 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+const PORT = 7865;
 
-const port = 7865;
+app.use(express.json());
 
-// Middleware to validate :id as a number
-app.param('id', (req, res, next, id) => {
-  if (!isNaN(id)) {
-    // If :id is a number, continue processing
-    next();
-  } else {
-    // If :id is not a number, return an error response
-    res.status(400).send('Invalid parameter :id. It must be a number.');
-  }
-});
-
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
-// Define a route for GET /cart/:id
-app.get('/cart/:id', (req, res) => {
-  // Access the validated :id from the request parameters
-  const { id } = req.params;
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
 
-  // Return the payment methods for the cart with the specified :id
   res.send(`Payment methods for cart ${id}`);
 });
 
-// Define a route for GET /available_payments
-app.get('/available_payments', (req, res) => {
-  // Return an object with payment methods
-  res.json({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  });
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-// Define a route for POST /login
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
+module.exports = app;
